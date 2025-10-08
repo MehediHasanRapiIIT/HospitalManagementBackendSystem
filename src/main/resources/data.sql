6cc143866cdc9fbaec7fa7479154075958984da7
MehediHasanRapiIIT <mehedi2515@student.nstu.edu.bd>
2025-10-08 06:55:02 +0600

    JPQL Native Pagination

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO patient (name, gender, birth_date, email, blood_group)
 VALUES
-    ('John Doe','male','1980-05-15','hasanrafi@gmail.com','A_POSITIVE'),
-    ('Jane Smith','male','1990-08-22','rafi@gmail.com','B_NEGATIVE'),
-    ('Alice Johnson','male','1975-12-03','hasan@gmail.com','O_POSITIVE');
+    ('Hasib','male','1981-05-15','hasanrafi@gmail.com','A_POSITIVE'),
+    ('Mehedi','male','1991-08-22','rafi@gmail.com','B_NEGATIVE'),
+    ('Rafi','male','1976-12-03','hasan@gmail.com','O_POSITIVE');
--- src/test/java/com/learning/hospitalmanagement/PatientTest.java
+++ src/test/java/com/learning/hospitalmanagement/PatientTest.java
@@ -1,11 +1,15 @@
 package com.learning.hospitalmanagement;
 
 import com.learning.hospitalmanagement.entity.Patient;
+import com.learning.hospitalmanagement.entity.type.BloodGroupType;
 import com.learning.hospitalmanagement.repository.PatientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -17,5 +21,19 @@ public class PatientTest {
     public void findAllPatients() {
        List<Patient> patientList = patientRepository.findAll();
         System.out.println(patientList);
+    }
+    @Test
+    public void testTransactionMethods(){
+     //   List<Patient> patientList = patientRepository.findBloodGroup(BloodGroupType.A_POSITIVE);
+     //   List<Patient> patientList = patientRepository.findByBornAfterDate(LocalDate.of(1990,1,1));
+        Page<Patient> patientList = patientRepository.findAllPatients(PageRequest.of(0, 2));
+        for(Patient patient: patientList){
+            System.out.println(patient);
+        }
+
+//        List<Object[]> bloodGroupCount = patientRepository.countBloodGroup();
+//        for(Object[] count : bloodGroupCount){
+//            System.out.println(count[0] + " : " + count[1]);
+//        }
     }
 }
